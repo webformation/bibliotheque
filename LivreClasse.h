67a1a92fde4416fb6fbbae2ea1bd1dd805e805f5
Dominique <dc@webformation.fr>
2021-12-12 18:16:07 +0100

    ajout classe LivreClasse

---
+++ LivreClasse.h
@@ -0,0 +1,22 @@
+#ifndef LIVRECLASSE_H_INCLUDED
+#define LIVRECLASSE_H_INCLUDED
+#include "Livre.h"
+
+class LivreClasse: public Livre
+{
+    std::string matiere;
+    std::string niveau;
+public:
+    LivreClasse(std::string titre, std::string matiere, std::string niveau ) :
+        Livre(titre, LCLASSE ),
+        matiere{matiere}, niveau {niveau} { }
+    LivreClasse(std::string titre, std::string auteur,std::string matiere, std::string niveau ) :
+        Livre(titre, LCLASSE,auteur ),
+        matiere{matiere}, niveau {niveau} { }
+    virtual std::string getDescription() override
+    {
+        return Livre::getDescription() + "," + matiere + ", " + niveau;
+    }
+};
+
+#endif // LIVRECLASSE_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Livre.h"
+#include "LivreClasse.h"
 using namespace std;
 
 int main()
@@ -12,5 +13,7 @@ int main()
     cout << boolalpha << (l1== l2) << endl;
     Livre l3("La Servante écarlate ", ROMAN,Auteur("Margaret Atwood"));
     cout << boolalpha << (l2== l3) << endl;
+    LivreClasse lc1{"Grammaire anglaise", "Langue", "Terminale"};
+    cout << lc1.getDescription() << endl;
     return 0;
 }
